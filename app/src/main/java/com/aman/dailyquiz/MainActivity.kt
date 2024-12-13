cd648056c197fe20ddea58ca9b426da7f14bfd7c
aman nishad <javaman0512@gmail.com>
2024-12-13 18:56:20 +0530

    bug fixed

--- app/src/main/java/com/aman/dailyquiz/MainActivity.kt
+++ app/src/main/java/com/aman/dailyquiz/MainActivity.kt
@@ -3,10 +3,7 @@ package com.aman.dailyquiz
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aman.dailyquiz.databinding.ActivityMainBinding
 import com.google.firebase.database.FirebaseDatabase
@@ -15,46 +12,66 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var quizModelList : MutableList<QuizModel>
     lateinit var adapter: QuizAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
 
-    }
         quizModelList = mutableListOf()
         getDataFromFirebase()
 
+
     }
 
-    private fun getDataFromFirebase() {
-        binding.progressBar.visibility = View.VISIBLE
-
-        FirebaseDatabase.getInstance().reference.get().addOnSuccessListener {
-            dataSnapshot ->
-
-            if(dataSnapshot.exists()){
-                for(snapshot in dataSnapshot.children){
-                    val quizModel = snapshot.getValue(QuizModel::class.java)
-                    if(quizModel!=null){
-                        quizModelList.add(quizModel)
-                        Log.i("Firebase Data", "$quizModel")
-                    }
-                }
-            }
-        }
-
-        setUpRecyclerView()
-    }
-
-    private fun setUpRecyclerView() {
+    private fun setupRecyclerView(){
+        Log.i("MainActivity", "Setting up RecyclerView with ${quizModelList.size} items")
         binding.progressBar.visibility = View.GONE
         adapter = QuizAdapter(quizModelList)
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
     }
+
+    private fun getDataFromFirebase(){
+        binding.progressBar.visibility = View.VISIBLE
+        FirebaseDatabase.getInstance().reference
+            .get()
+            .addOnSuccessListener { dataSnapshot->
+                Log.d("Firebase Debug", "Data Snapshot: ${dataSnapshot.value}")
+
+                if(dataSnapshot.exists()){
+
+                    for (snapshot in dataSnapshot.children){
+                        val quizModel = snapshot.getValue(QuizModel::class.java)
+                        if (quizModel != null) {
+                            quizModelList.add(quizModel)
+                            Log.d("Firebase Data", "QuizModel: $quizModel")
+                        }
+                    }
+                } else {
+                    Log.w("Firebase Data", "No data found in Firebase")
+                }
+                setupRecyclerView()
+            }
+            .addOnFailureListener { exception ->
+                binding.progressBar.visibility = View.GONE
+                Log.e("Firebase Error", "Error fetching data", exception)
+
+            }
+
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
